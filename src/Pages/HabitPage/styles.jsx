a6d90028989b57a6b45e332c18d3761307084a27
Maicon Campbell <maicon_campbell@hotmail.com>
2022-11-30 20:23:02 -0300

    ajuste

--- src/Pages/HabitPage/styles.jsx
+++ src/Pages/HabitPage/styles.jsx
@@ -4,7 +4,7 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'rgba(21,21,21,.98'
+    backgroundColor: 'rgba(21,21,21,.98)'
   },
   backPageBtn: {
     width: 40,
